9fd7cde41229c66d74fe011bcb0123d185888bc1
tianfeng <1114089877@qq.com>
2018-08-31 23:29:37 +0800

    就是提交代码的啊

--- src/main/java/com/monlie/web/controller/UserController.java
+++ src/main/java/com/monlie/web/controller/UserController.java
@@ -2,10 +2,10 @@ package com.monlie.web.controller;
 
 import com.monlie.web.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -15,7 +15,7 @@ import java.util.Map;
  *
  * @author monlie
  */
-@Controller
+@RestController
 public class UserController {
 
     @Autowired
@@ -29,10 +29,8 @@ public class UserController {
 
     @RequestMapping("/indexPage")
     public String helloFtl(HttpServletRequest request, Map<String, Object> map) {
-
-        String method = request.getMethod();
-        System.err.print(method);
-        //Thread.dumpStack();
+        Thread.State state = Thread.currentThread().getState();
+        System.err.print(state);
         map.put("desc", userRepository.getUserInfo());
         map.put("hello", "Hello FreeMarker");
         return "index";
